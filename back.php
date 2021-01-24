8754cf697d1ad781a16f2f857679851b5b72d0fe
gaonispildoras <srgaona1@gmail.com>
2021-01-24 01:34:15 +0100

    Quitamos las funciones login y registro ya que la hemos hecho aparte, también la de conexion, ahora hecha con PDO

--- back.php
+++ back.php
@@ -3,25 +3,15 @@
 class Usuarios{
 
     //Atributos
-    public $conexion="";
+
 
 
     //Métodos
 
     public function __construct(){
-        $host="localhost";
-        $db="crud";
-        $user="root";
-        $pass="";
-        
-        $conexion=mysqli_connect($host,$user,$pass,$db);
-        $this->conexion=$conexion;                  
+             
     }
  
-
-    function registro(){
-
-    }
 
     function eliminar(){
 
@@ -33,52 +23,8 @@ class Usuarios{
     }
 
 
-    function vacio_login(){
-        if ($usuario=""){
 
-            return true;
-            
-            
 
-        }
-        else{
-            
-            return false;
-        }
-    }
-
-    function registro_login($usuario, $contraseña, $contraseña2, $correo, $admin){
-        if($this->vacio_login()==false){
-
-            if($admin=="normal"){
-                if ($contraseña==$contraseña2){
-                    $sql="INSERT INTO usuarios (usuario, contraseña, correo) VALUES ('$usuario', '$contraseña', '$correo')";
-                    mysqli_query($this->conexion, $sql);
-                    echo "Usuario registrado con éxito";
-                }
-                else{
-                    echo "Las contraseñas no coinciden";
-                }
-                
-            }
-            else{
-                if ($contraseña==$contraseña2){
-                    $sql="INSERT INTO admin (usuario, contraseña, correo) VALUES ('$usuario', '$contraseña', '$correo')";
-                    mysqli_query($this->conexion, $sql);
-                    echo "Usuario registrado con éxito";
-                }
-                else{
-                    echo "Las contraseñas no coinciden";
-                }
-
-            }
-
-        }
-        else{
-            echo "debes rellenar todos los campos";
-        }
-
-    }
 
 
 }
