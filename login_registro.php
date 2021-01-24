27e8df106cd8356f323e4bc7b3ee1427c8c77eaf
gaonispildoras <srgaona1@gmail.com>
2021-01-24 01:36:06 +0100

    Hemos separado el login y el registro en esta clase, todas las consultas a bbdd están hechas con PDO

--- login_registro.php
+++ login_registro.php
@@ -1,30 +1,80 @@
 <?php
 include("conexion.php");
 
-class Login_registro{
+class Login_registro extends Conexion{
 
-    public $con;
+   
+
+   public function __construct(){
+
+   }
+
+   public function registrar($usuario,$pass,$pass2,$correo,$admin){
+      if($this->vacio_login()==false){
+
+         if($admin=="normal"){
+            if ($pass==$pass2){
+               $sql="INSERT INTO usuarios (usuario , contraseña, correo) VALUES (:usuario , :pass, :correo)";
+               $resultado = Conexion::__construct()->prepare($sql);
+               $resultado->execute(array(":usuario"=>$usuario,":pass"=>$pass,":correo"=>$correo));
+               
+            }
+            else{
+               echo "Las contraseñas no coinciden";
+            }
+            
+         }
+         else{
+            if ($pass==$pass2){
+               $sql="INSERT INTO admin2 (usuario , contraseña, correo) VALUES (:usuario , :pass, :correo)";
+               $resultado = Conexion::__construct()->prepare($sql);
+               $resultado->execute(array(":usuario"=>$usuario,":pass"=>$pass,":correo"=>$correo));
+            }
+            else{
+               echo "Las contraseñas no coinciden";
+            }
+
+         }
+
+      }
+      else{
+            echo "debes rellenar todos los campos";
+      }
+   }
+   public function login($usuario, $pass){
+      $sql="SELECT * FROM usuarios WHERE usuario=:usuario AND contraseña=:pass";
+      $sql2="SELECT * FROM admin2 WHERE usuario=:usuario AND contraseña=:pass";
+
+      $resultado = Conexion::__construct()->prepare($sql);
+      $resultado->execute(array(":usuario"=>$usuario, ":pass"=>$pass));
+
+      $resultado2 = Conexion::__construct()->prepare($sql);
+      $resultado2->execute(array(":usuario"=>$usuario, ":pass"=>$pass));
+
+      if ($resultado->rowCount()==1){
+         header("Location: index.php");
+      }
+      elseif($resultado2->rowCount()==1){
+         header("Location: index.php");
+      }
+      else{
+         echo "El nombre de usuario o la contraseña no coinciden";
+      }
 
 
- public function __construct($dni,$nombre,$apellido){
-    $this->con = new Conexion;
-    $sql="INSERT INTO crud (dni , nombre) VALUES ('$dni', '$nombre', '$apellido')";
-    mysqli_query($this->con, $sql);
+   }
 
- }
+   public function vacio_login(){
+      if ($usuario=""){ 
 
- public function crear(){
+         return true;
+      }
+      else{    
 
-
-
- }
-
-
-
+         return false;
+      }
+   }      
 
 }
-$hola = new Login_registro(3555, "hola", "buenas");
-
-
 
 ?>
