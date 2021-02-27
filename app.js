1e88cb3afecd78e8a1c7a7e1d5fe0448485bd3fb
gaonispildoras <srgaona1@gmail.com>
2021-02-27 22:04:07 +0100

    Se añade la funcion que edita los datos al pulsar 'guardar cambios' en el modal

--- app.js
+++ app.js
@@ -1,23 +1,46 @@
-//FUNCION EDITAR
+
 $(function(){ 
+    //Ocultar info
+    //$(".oculto").hide();
+
+    
+    //PASAR DATOS AL MODAL
 
     $(".editar").on('click', function(){
         
         var prueba = $(this).parents("tr").find("td");
         var prueba1 = [];
-        prueba.each(function( index ) {
+        prueba.each(function() {
             prueba1.push($( this ).text());
-            
-            
+                        
         });
+
         $("#nombre").val(prueba1[0]);      
         $("#apellidos").val(prueba1[1]);    
         $("#edad").val(prueba1[2]);
         $("#correo").val(prueba1[3]);
         $("#direccion").val(prueba1[4]);
+        $("#id_us").val(prueba1[5]);
+
+        
         
 
         
+    });
+
+    //EDITAR LOS DATOS
+    
+    
+//
+    $(".cambios").on("click", function(){
+        var datos = [];
+        $(".datos").each(function() {
+            datos.push($(this).val());
+
+            
+        });
+        var cargar = "calls_js/update.php?nom="+datos[0]+"&ape="+datos[1]+"&edad="+datos[2]+"&corr="+datos[3]+"&dire="+datos[4]+"&oculto="+datos[5]+" ";
+        $("#hola1").load(cargar);
     });
 
 });
